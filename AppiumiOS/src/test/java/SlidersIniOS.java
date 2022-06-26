97efd8eb87d4942cc3f9315e20b322bc2b077e05
smahuzaifa <smahuzaifa@localhost>
2022-06-26 13:36:14 +0530

    Updated comments

--- AppiumiOS/src/test/java/SlidersIniOS.java
+++ AppiumiOS/src/test/java/SlidersIniOS.java
@@ -13,12 +13,22 @@ public class SlidersIniOS extends DesiredCapabilitiesiOS {
 		driver.findElementByAccessibilityId("Sliders").click();
 		IOSElement slider =(IOSElement)driver.findElementByXPath("//XCUIElementTypeSlider");
 		Thread.sleep(2000);
+		/*
+		 * Sliding and all are iOS specific so we make use of IOSElement class to get the extra
+		 * iOS specific functionality
+		 * Above, we have cast the element to iOSElement (since it initially behaves as webelement
+		 */
 		slider.setValue("0%");
 		Thread.sleep(2000);
 		System.out.println(slider.getAttribute("value"));
 		slider.setValue("1%");
 		Thread.sleep(2000);
 		System.out.println(slider.getAttribute("value"));
+		/*
+		 * Setvalue method accepts value between 0 to 1
+		 * to set value of 42 we need 0.42
+		 * Similarly to set 100 we need 1
+		 */
 		Assert.assertEquals("100%",slider.getAttribute("value"));
 	}
 
