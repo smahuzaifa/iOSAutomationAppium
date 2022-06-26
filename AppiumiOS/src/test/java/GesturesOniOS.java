5f16008a8f49401f2fdc6648d8833c28ffe26c7a
smahuzaifa <smahuzaifa@localhost>
2022-06-26 19:34:20 +0530

    Added tapOptions to gestures Added notes Added real device automation help doc

--- AppiumiOS/src/test/java/GesturesOniOS.java
+++ AppiumiOS/src/test/java/GesturesOniOS.java
@@ -6,6 +6,7 @@ import io.appium.java_client.ios.IOSTouchAction;
 import static io.appium.java_client.touch.LongPressOptions.longPressOptions;
 import static io.appium.java_client.touch.offset.ElementOption.element;
 import static java.time.Duration.ofSeconds;
+import static io.appium.java_client.touch.TapOptions.tapOptions;
 
 public class GesturesOniOS extends DesiredCapabilitiesForLongTap{
 
@@ -13,10 +14,17 @@ public class GesturesOniOS extends DesiredCapabilitiesForLongTap{
 		// TODO Auto-generated method stub
 		IOSDriver driver= DesiredCapabilities2();
 		//Gesture is specific to mobile
+		//LongPress
 		MobileElement e = (MobileElement)driver.findElementByXPath("//XCUIElementTypeStaticText[@name=\"Long tap\"]");
 		IOSTouchAction touch= new IOSTouchAction(driver);
 		touch.longPress(longPressOptions().withElement(element(e)).withDuration(ofSeconds(2))).release().perform();
 		//We have to release after a long press
+		//Tap
+		MobileElement tap = (MobileElement)driver.findElementByXPath("//XCUIElementTypeSwitch[1]");
+		touch.tap(tapOptions().withElement(element(tap))).perform();
+		//Since tap is just a single action we do not have to release it, we directly perform
+		MobileElement tap2 = (MobileElement)driver.findElementByXPath("//XCUIElementTypeSwitch[2]");
+		touch.tap(tapOptions().withElement(element(tap2))).perform();
 	}
 
 }
