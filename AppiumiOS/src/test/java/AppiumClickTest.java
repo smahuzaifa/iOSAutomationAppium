fa18fea00f38760cbc3ca6e08ceef16bae333632
smahuzaifa <smahuzaifa@localhost>
2022-06-26 11:13:07 +0530

    Updated pom and also added first basic class to automate ios app

---
+++ AppiumiOS/src/test/java/AppiumClickTest.java
@@ -0,0 +1,27 @@
+import java.net.MalformedURLException;
+
+import io.appium.java_client.ios.IOSDriver;
+
+public class AppiumClickTest extends DesiredCapabilitiesiOS {
+
+	public static void main(String[] args) throws MalformedURLException {
+		IOSDriver driver = DesiredCapabilities();
+		driver.findElementByAccessibilityId("Alert Views").click();
+		//driver.findElementByAccessibilityId("Text Entry").click();
+		driver.findElementByXPath("//XCUIElementTypeStaticText[@name=\"Text Entry\"]").click(); 
+		driver.findElementByXPath("//XCUIElementTypeCell").sendKeys("Hello");
+		/*
+		 * When unique id is not available we make use of type or class type in xpath ignoring
+		 * the atrribute part
+		 */
+		driver.findElementByXPath("//XCUIElementTypeButton[@name=\"OK\"]").click();
+		driver.findElementByXPath("//XCUIElementTypeStaticText[@name=\"Confirm / Cancel\"]").click();
+		System.out.println(driver.findElementByXPath("//*[contains(@name,'message')]").getText());
+		/*
+		 * What above expression does is, in the whole view it will find if it sees any "message" keyword
+		 * in name attribute. if it finds then it will get the whole text present in that attribute
+		 */
+		driver.findElementByXPath("//XCUIElementTypeButton[@name=\"Confirm\"]").click();
+	}
+
+}
--- AppiumiOS/src/test/java/DesiredCapabilitiesiOS.java
+++ AppiumiOS/src/test/java/DesiredCapabilitiesiOS.java
@@ -1,7 +1,6 @@
 import java.net.MalformedURLException;
 import java.net.URL;
 
-import org.apache.log4j.xml.DOMConfigurator;
 import org.openqa.selenium.remote.DesiredCapabilities;
 
 import io.appium.java_client.ios.IOSDriver;
@@ -9,9 +8,9 @@ import io.appium.java_client.remote.AutomationName;
 import io.appium.java_client.remote.MobileCapabilityType;
 import io.appium.java_client.remote.IOSMobileCapabilityType;
 
-public class BasiciOSTest {
-	
-	public static void main(String[] args) throws MalformedURLException {
+public class DesiredCapabilitiesiOS {
+	public static IOSDriver DesiredCapabilities() throws MalformedURLException
+	{
 		DesiredCapabilities c = new DesiredCapabilities();
 		c.setCapability(MobileCapabilityType.PLATFORM_VERSION, "15.2");
 		c.setCapability(MobileCapabilityType.DEVICE_NAME, "iPhone 13 Pro Max");
@@ -21,6 +20,8 @@ public class BasiciOSTest {
 		c.setCapability("commandTimeouts", "12000");
 		c.setCapability(MobileCapabilityType.APP, "Users/smahuzaifa/Downloads/Appium File and Folders/UIKitCatalog.app");
 		IOSDriver driver = new IOSDriver(new URL("http://localhost:4723/wd/hub"),c);
+		return driver;
 	}
 
 }
+ 
